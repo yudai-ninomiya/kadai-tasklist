7cfb2b29f5b8b5b651f380e3ae05fea04f428251
yudai.ninomiya <n.yudai.1016.y@gmail.com>
2020-04-25 09:21:01 +0000

    kadai-1

---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>メッセージ一覧</h1>
+
+    @if (count($tasks) > 0)
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>メッセージ</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($tasks as $task)
+                <tr>
+                    <td>{{ $task->id }}</td>
+                    <td>{{ $task->content }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @endif
+
+@endsection

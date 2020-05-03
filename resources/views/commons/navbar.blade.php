4a2302ffb7f0d4bf31b7ec1402bc0d2839d52029
yudai.ninomiya <n.yudai.1016.y@gmail.com>
2020-05-03 04:54:20 +0000

    registration

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -10,7 +10,7 @@
                     <ul class="navbar-nav mr-auto"></ul>
                     <ul class="navbar-nav">
                         
-                        <li class="nav-item"><a href="#" class="nav-link">Signup</a></li>
+                        <li class="nav-item">{!! link_to_route('signup.get', 'Signup', [], ['class' => 'nav-link']) !!}</li>
                         <li class="nav-item"><a href="#" class="nav-link">Login</a></li>
                     </ul>
                 </div>
